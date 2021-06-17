1a959eb5254768897510227ac733330799c9a738
Ale Madic <aleksandar.madic.mes@gmail.com>
2021-06-17 20:10:45 +0200

    final

--- NewsBlog.Implementation/Validators/PostRating/CreatePostRatingValidator.cs
+++ NewsBlog.Implementation/Validators/PostRating/CreatePostRatingValidator.cs
@@ -15,7 +15,11 @@ namespace NewsBlog.Implementation.Validators.PostRating
         {
             RuleFor(x => x.Rating).NotEmpty()
                 .Must(x => x >= 1 && x <= 5)
-                .WithMessage("Provide number between 1 and 5");
+                .WithMessage("Provide number between 1 and 5.");
+
+            RuleFor(x => x.UserId)
+                .Must((pr, userId) => !context.PostRatings.Any(x => x.PostId == pr.PostId && x.UserId == pr.UserId))
+                .WithMessage("You already rated this post.");
 
             //RuleFor(x => x.UserId).NotEmpty()
             //    .Must(x => context.Users.Any(u => u.Id == x))
